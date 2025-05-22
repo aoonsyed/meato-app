d28911f0ed6d536b7c2325e9c783b72fa7b319a8
Ahmad <168859609+ahmad0iq@users.noreply.github.com>
2025-05-22 16:53:15 +0500

    Fix sell with us badges.

--- src/components/FeatureBadge.jsx
+++ src/components/FeatureBadge.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import checkIcon from "../assets/landingPage/checkIcon.png";
+import { FaCheckCircle } from 'react-icons/fa';
 
 function FeatureBadge({ text, className }) {
   return (
-    <div className={`bg-[#FFBD59] text-white rounded-xl py-4 px-8 flex items-center justify-center gap-2 ${className}`}>
-      <img src={checkIcon} alt="Icon" />
-      <span>{text}</span>
+    <div className={`bg-gradient-to-r from-red-700 to-red-900 text-white rounded-xl py-4 px-6 flex items-center justify-center gap-3 shadow-md hover:shadow-lg transition-shadow duration-300 ${className}`}>
+      <FaCheckCircle className="text-green-400 text-xl" />
+      <span className='font-medium'>{text}</span>
     </div>
   );
 }
